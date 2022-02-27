b59714a6795d6eca22064e923688e958c1a7d89d
seong-dodo <dyrnxm52@hanmail.net>
2022-02-27 17:41:22 +0900

    키보드 이벤트

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,7 +1,10 @@
 export class keyboard {
+  #containerEl;
   #swichEl;
   #fontSelectEl;
-  #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -11,11 +14,38 @@ export class keyboard {
     this.#containerEl = document.getElementById("container");
     this.#swichEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
     this.#swichEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+    this.#inputEl.addEventListener("input", this.#onInput);
+  }
+
+  #onInput(e) {
+    e.target.value = e.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+  }
+
+  #onKeyUp(e) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${e.code}]`)
+      ?.classList.remove("active");
+  }
+
+  #onKeyDown(e) {
+    this.#inputGroupEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(e.key)
+    );
+
+    this.#keyboardEl
+      .querySelector(`[data-code=${e.code}]`)
+      ?.classList.add("active");
   }
 
   #onChangeTheme(e) {
